1948cd5bda01a5f78540ee1286fd3d65bec601ac
logiph <sosend2009@gmail.com>
2016-07-18 18:41:02 +0800

    提交小行星，可以随机旋转的功能

---
+++ MySpaceShooter/Assets/_Script/RandomRotator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+		// Random.insideUnitSphere 返回一个单位长度的球体内的一个随机点
+		// angularVelocity 角速度
+		GetComponent<Rigidbody> ().angularVelocity = Random.insideUnitSphere * tumble;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+
+
+
+}
