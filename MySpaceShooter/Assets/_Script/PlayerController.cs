9b32f220aad8f9714d712c86e6c1b47dbd8341f4
logiph <sosend2009@gmail.com>
2016-07-18 16:12:26 +0800

    添加移动的时候边界检测，模型倾斜。

--- MySpaceShooter/Assets/_Script/PlayerController.cs
+++ MySpaceShooter/Assets/_Script/PlayerController.cs
@@ -1,9 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5.0f;
+	public Boundary boundary;
+	public float tilt = 4.0f;
 
 
 	// Use this for initialization
@@ -23,10 +30,23 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
 
-		GetComponent<Rigidbody> ().velocity = movement*speed;
+
+		Rigidbody rb = GetComponent<Rigidbody> ();
+
+		if (rb != null) {
+
+			rb.velocity = movement * speed;
+
+			rb.position = new Vector3 (Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+				0,
+				Mathf.Clamp(rb.position.y, boundary.zMin, boundary.zMax));
 
 
-		
+			rb.rotation = Quaternion.Euler(0, 0, rb.velocity.x*-tilt);
+
+
+
+		}		
 	}
 
 }
