b228c30657605b7e961a2c924ddf1d88568a35af
logiph <sosend2009@gmail.com>
2016-07-18 18:55:12 +0800

    添加小行星和子弹碰撞之后两者都销毁的检测脚本。

---
+++ MySpaceShooter/Assets/_Script/DestoryByContact.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryByContact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+	
+		if (other.tag == "Boundary") {
+			return;
+		}
+
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+
+
+
+}
