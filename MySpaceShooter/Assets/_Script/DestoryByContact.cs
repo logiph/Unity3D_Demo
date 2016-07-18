cddff52a913307331f0b1b305753f47e3ca629c6
logiph <sosend2009@gmail.com>
2016-07-18 19:05:41 +0800

    添加子弹与小行星发生碰撞之后爆炸的粒子效果

--- MySpaceShooter/Assets/_Script/DestoryByContact.cs
+++ MySpaceShooter/Assets/_Script/DestoryByContact.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class DestoryByContact : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,8 +19,15 @@ public class DestoryByContact : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 	
+		// 防止启动的时候就发生了小行星和边框的碰撞
 		if (other.tag == "Boundary") {
 			return;
+		}
+
+		Instantiate (explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") {		
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 		}
 
 		Destroy (other.gameObject);
