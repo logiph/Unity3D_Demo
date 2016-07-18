4971d7764c42ab3f238e89668d9cdc55790ad438
logiph <sosend2009@gmail.com>
2016-07-18 16:32:27 +0800

    提交子弹发射的 预设体，以及代码

---
+++ MySpaceShooter/Assets/_Script/Mover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+
+		float moveHorizontal = 0;
+		float moveVertical = 2;
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.velocity = movement * speed;
+			
+	}
+
+
+}
