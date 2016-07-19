608a50d7e0362adf549069cec934ce7253614d77
logiph <sosend2009@gmail.com>
2016-07-19 10:27:45 +0800

    设置小行星的位置从随机的位置产生.

---
+++ MySpaceShooter/Assets/_Script/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	private Vector3 spawnPosition = Vector3.zero;
+	private Quaternion spawnRotation;
+
+	// Use this for initialization
+	void Start () {
+	
+		SpawnWaves ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnWaves() {
+
+		spawnPosition.x = Random.Range (-spawnValues.x, spawnValues.x);
+		spawnPosition.z = spawnValues.z;
+		spawnRotation = Quaternion.identity;
+
+		Instantiate (hazard, spawnPosition, spawnRotation);
+
+	}
+
+}
