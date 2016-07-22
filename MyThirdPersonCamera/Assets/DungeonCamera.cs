b4f98c3b35a536b4674c932b53596285e69d07bb
logiph <sosend2009@gmail.com>
2016-07-22 15:16:13 +0800

    设置第三人称的摄像头的工程，

---
+++ MyThirdPersonCamera/Assets/DungeonCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonCamera : MonoBehaviour {
+
+	public GameObject target;
+	public float damping = 1;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+	
+		offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate() {
+
+		Vector3 desiredPosition = target.transform.position + offset;
+		Vector3 position = Vector3.Lerp (transform.position, desiredPosition, Time.deltaTime * damping);
+
+		transform.position = position;
+		transform.LookAt (target.transform.position);
+	}
+
+}
