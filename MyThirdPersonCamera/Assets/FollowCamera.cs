838e72946d2bd9e635ec457a714136e78bfb5b4a
logiph <sosend2009@gmail.com>
2016-07-22 16:21:52 +0800

    添加旋转第三人称的时候，镜头缓慢移动的效果。

--- MyThirdPersonCamera/Assets/FollowCamera.cs
+++ MyThirdPersonCamera/Assets/FollowCamera.cs
@@ -5,6 +5,7 @@ public class FollowCamera : MonoBehaviour {
 
 	public GameObject target;
 	Vector3 offset;
+	public float damping = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -18,14 +19,19 @@ public class FollowCamera : MonoBehaviour {
 	}
 
 	void LateUpdate() {
-
+//
+//		float desiredAngle = target.transform.eulerAngles.y;
+//		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+//
+//		transform.position = target.transform.position - (rotation * offset);
+//		transform.LookAt (target.transform);
+		float currentAngle = transform.eulerAngles.y;
 		float desiredAngle = target.transform.eulerAngles.y;
-		Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
+		float angle = Mathf.LerpAngle (currentAngle, desiredAngle, Time.deltaTime * damping);
 
+		Quaternion rotation = Quaternion.Euler (0, angle, 0);
 		transform.position = target.transform.position - (rotation * offset);
+
 		transform.LookAt (target.transform);
-
-
-
 	}
 }
